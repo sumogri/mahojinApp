ebc8df84df7ff3f55441f1b6b36158463590a17d
sumogri <kazusi@hotmail.co.jp>
2017-07-24 16:39:20 +0900

    [add] 魔方陣を作るクラスを追加

---
+++ mahojin/Assets/Mahojin/Scripts/GameMain/MagicSquareMaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicSquareMaker : SingletonMonoBehaviour<MagicSquareMaker> {
+    [SerializeField] private GameObject magicSquare;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
