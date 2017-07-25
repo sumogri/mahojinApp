0ad5592ef67f6469c18e9f22042897933dbb3407
sumogri <kazusi@hotmail.co.jp>
2017-07-25 17:42:46 +0900

    [add] 魔方陣の各セル参照を追加

--- mahojin/Assets/Mahojin/Scripts/GameMain/MagicSquareMaker.cs
+++ mahojin/Assets/Mahojin/Scripts/GameMain/MagicSquareMaker.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MagicSquareMaker : SingletonMonoBehaviour<MagicSquareMaker> {
     [SerializeField] private GameObject magicSquare;
+    private InputField[] msFields;  //魔方陣のセル
 
 	// Use this for initialization
 	void Start () {
-		
+        msFields = magicSquare.GetComponentsInChildren<InputField>();
+        for(int i = 0; i < msFields.Length; i++)
+        {
+            msFields[i].text = "" + i;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private int[] Assume(int[] cells)
+    {
+        cells[2] = 1;
+        Debug.Log(cells);
+
+        return cells;
+    }
 }
