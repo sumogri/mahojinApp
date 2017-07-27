856ceff7e1dc76eb76b73457e5568aad01454fab
sumogri <kazusi@hotmail.co.jp>
2017-07-27 17:48:08 +0900

    [add] 魔方陣のスクリーンショットをとる機能を追加

--- mahojin/Assets/Mahojin/Scripts/GameMain/MagicSquareMaker.cs
+++ mahojin/Assets/Mahojin/Scripts/GameMain/MagicSquareMaker.cs
@@ -4,17 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 4次方陣を作成するクラス
+/// </summary>
 public class MagicSquareMaker : SingletonMonoBehaviour<MagicSquareMaker> {
     [SerializeField] private GameObject magicSquare;
     private InputField[] msFields;  //魔方陣のセル
 
 	// Use this for initialization
 	void Start () {
-        msFields = magicSquare.GetComponentsInChildren<InputField>();
-        for(int i = 0; i < msFields.Length; i++)
-        {
-            msFields[i].text = "" + i;
-        }
+        msFields = magicSquare.GetComponentsInChildren<InputField>();        
 	}
 	
 	// Update is called once per frame
@@ -24,6 +23,7 @@ public class MagicSquareMaker : SingletonMonoBehaviour<MagicSquareMaker> {
 
     private int[] Assume(int[] cells)
     {
+        int sum = cells[0] + cells[1] + cells[2];
         return cells;
     }
 }
---
+++ mahojin/Assets/Mahojin/Scripts/Util/ScreenShotMaker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotMaker : MonoBehaviour{
+    public void MakeScreenShot()
+    {
+        Application.CaptureScreenshot("Screenshot.png",4);
+    }
+}
