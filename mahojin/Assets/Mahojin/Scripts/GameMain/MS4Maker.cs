2835181dda18222fa6ed9c801dd8af5fdfbbdc87
sumogri <kazusi@hotmail.co.jp>
2017-08-10 10:59:12 +0900

    [update] リネーム

--- mahojin/Assets/Mahojin/Scripts/GameMain/MS4Maker.cs
+++ mahojin/Assets/Mahojin/Scripts/GameMain/MS4Maker.cs
@@ -8,11 +8,12 @@ using UnityEngine;
 /// 4次方陣を作成する関数群
 /// </summary>
 public static class MS4Maker{
-    private static Func<int?[], int, int?>[][] fillFuncs = new Func<int?[], int, int?>[16][];
+    //あるセルに入るべき数を、定和からの減算で求める式群
+    private static Func<int?[], int, int?>[][] cellFuncs = new Func<int?[], int, int?>[16][];
 
     static MS4Maker()
     {
-        fillFuncsInit();
+        InitCellFuncs();
     }
 
     /// <summary>
@@ -29,7 +30,7 @@ public static class MS4Maker{
             
             if (cells[i] != null) continue;
 
-            var assumes = fillFuncs[i].Select((x) => { return x(cells, sum); })
+            var assumes = cellFuncs[i].Select((x) => { return x(cells, sum); })
                 .Where(x => x.HasValue == true).Distinct().ToArray();
 
             //解が求まる中で唯一になる場合
@@ -47,102 +48,102 @@ public static class MS4Maker{
     /// e.g.)   一行目の各セルの数をa-dとする
     ///         a = sum - b - c - d
     /// </summary>
-    private static void fillFuncsInit()
+    private static void InitCellFuncs()
     {
-        if (fillFuncs[0] != null) return;
+        if (cellFuncs[0] != null) return;
 
         //セルごとに使う等式を設定
-        fillFuncs[0] = new Func<int?[], int, int?>[] {
+        cellFuncs[0] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[1] + c[2] + c[3]),
             (c,s) => s - (c[1+4] + c[2+4*2] + c[3+4*3]),
             (c,s) => s - (c[4] + c[4*2] + c[4*3]),
             (c,s) => s - (c[3] + c[4*3] + c[3+4*3])
         };
-        fillFuncs[1] = new Func<int?[], int, int?>[] {
+        cellFuncs[1] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[0] + c[2] + c[3]),
             (c,s) => s - (c[1+4] + c[1+4*2] + c[1+4*3]),
             (c,s) => s - (c[2] + c[1+4*3] + c[2+4*3]),
         };
-        fillFuncs[2] = new Func<int?[], int, int?>[] {
+        cellFuncs[2] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[0] + c[1] + c[3]),
             (c,s) => s - (c[2+4] + c[2+4*2] + c[2+4*3]),
             (c,s) => s - (c[1] + c[1+4*3] + c[2+4*3]),
         };
-        fillFuncs[3] = new Func<int?[], int, int?>[] {
+        cellFuncs[3] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[1] + c[2] + c[0]),
             (c,s) => s - (c[2+4] + c[1+4*2] + c[4*3]),
             (c,s) => s - (c[3+4] + c[3+4*2] + c[3+4*3]),
             (c,s) => s - (c[0] + c[4*3] + c[3+4*3])
         };
-        fillFuncs[4] = new Func<int?[], int, int?>[]
+        cellFuncs[4] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[1+4] + c[2+4] +c[3+4]),
             (c,s) => s - (c[0] + c[4*2] + c[4*3]),
             (c,s) => s - (c[4*2] + c[3+4] + c[3+4*2])
         };
-        fillFuncs[5] = new Func<int?[], int, int?>[]
+        cellFuncs[5] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4] + c[2+4] + c[3+4]),
             (c,s) => s - (c[0] + c[2+4*2] + c[3+4*3]),
             (c,s) => s - (c[1] + c[1+4*2] + c[1+4*3]),
             (c,s) => s - (c[2+4] + c[1+4*2] + c[2+4*2]),
         };
-        fillFuncs[6] = new Func<int?[], int, int?>[]
+        cellFuncs[6] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4] + c[1+4] + c[3+4]),
             (c,s) => s - (c[3] + c[1+4*2] + c[4*3]),
             (c,s) => s - (c[2] + c[2+4*2] + c[2+4*3]),
             (c,s) => s - (c[1+4] + c[1+4*2] + c[2+4*2]),
         };
-        fillFuncs[7] = new Func<int?[], int, int?>[]
+        cellFuncs[7] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4] + c[1+4] + c[2+4]),
             (c,s) => s - (c[3] + c[3+4*2] + c[3+4*3]),
             (c,s) => s - (c[4] + c[4*2] + c[3+4*2]),
         };
-        fillFuncs[8] = new Func<int?[], int, int?>[]
+        cellFuncs[8] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[1+4*2] + c[2+4*2] +c[3+4*2]),
             (c,s) => s - (c[0] + c[4*1] + c[4*3]),
             (c,s) => s - (c[4] + c[3+4] + c[3+4*2])
         };
-        fillFuncs[9] = new Func<int?[], int, int?>[]
+        cellFuncs[9] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4*2] + c[2+4*2] + c[3+4*2]),
             (c,s) => s - (c[3] + c[2+4] + c[4*3]),
             (c,s) => s - (c[1] + c[1+4] + c[1+4*3]),
             (c,s) => s - (c[2+4] + c[1+4] + c[2+4*2]),
         };
-        fillFuncs[10] = new Func<int?[], int, int?>[]
+        cellFuncs[10] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4*2] + c[1+4*2] + c[3+4*2]),
             (c,s) => s - (c[0] + c[1+4] + c[3+4*3]),
             (c,s) => s - (c[2] + c[2+4] + c[2+4*3]),
             (c,s) => s - (c[1+4] + c[1+4*2] + c[2+4]),
         };
-        fillFuncs[11] = new Func<int?[], int, int?>[]
+        cellFuncs[11] = new Func<int?[], int, int?>[]
         {
             (c,s) => s - (c[4*2] + c[1+4*2] + c[2+4*2]),
             (c,s) => s - (c[3] + c[3+4] + c[3+4*3]),
             (c,s) => s - (c[4] + c[4*2] + c[3+4]),
         };
-        fillFuncs[12] = new Func<int?[], int, int?>[] {
+        cellFuncs[12] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[1+4*3] + c[2+4*3] + c[3+4*3]),
             (c,s) => s - (c[3] + c[2+4] + c[1+4*2]),
             (c,s) => s - (c[4] + c[4*2] + c[0]),
             (c,s) => s - (c[3] + c[0] + c[3+4*3])
         };
-        fillFuncs[13] = new Func<int?[], int, int?>[] {
+        cellFuncs[13] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[0+4*3] + c[2+4*3] + c[3+4*3]),
             (c,s) => s - (c[1+4] + c[1+4*2] + c[1]),
             (c,s) => s - (c[2] + c[1] + c[2+4*3]),
         };
-        fillFuncs[14] = new Func<int?[], int, int?>[] {
+        cellFuncs[14] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[0+4*3] + c[1+4*3] + c[3+4*3]),
             (c,s) => s - (c[2+4] + c[2+4*2] + c[2]),
             (c,s) => s - (c[1] + c[1+4*3] + c[2]),
         };
-        fillFuncs[15] = new Func<int?[], int, int?>[] {
+        cellFuncs[15] = new Func<int?[], int, int?>[] {
             (c,s) => s - (c[1+4*3] + c[2+4*3] + c[0+4*3]),
             (c,s) => s - (c[0] + c[1+4] + c[2+4*2]),
             (c,s) => s - (c[3+4] + c[3+4*2] + c[3]),
@@ -169,10 +170,13 @@ public static class MS4Maker{
         //―――――――――――――――――
         //| c + g | d + h | a + f | b + e |
         //―――――――――――――――――
-        // これに従って、行列対角隅端どれか4つが埋まったら
+        // これに従って、行列対角隅端どれか4つが埋まったら、その他のセルを埋める。
 
-        var nums = cells.Where(x => x.HasValue);
+        //もし、
+        for(int i = 0; i < 4; i++)
+        {
 
+        }
 
 
         return cells;
