e0dd58ed395244eb96f2947f8ab372fba034e0ec
sumogri <kazusi@hotmail.co.jp>
2017-08-17 18:17:01 +0900

    [add] タイトルシーンを追加

---
+++ mahojin/Assets/Mahojin/Scripts/Util/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シーンを遷移させる機能
+/// </summary>
+public class SceneChanger : SingletonMonoBehaviour<SceneChanger>{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
